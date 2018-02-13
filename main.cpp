0b498364ed96ebee04f5bc55be4cd4f95feaf47d
corozco10 <corozco10@mail.csuchico.edu>
2018-02-13 11:54:24 -0800

    Lucky number

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ int main()
 {
   //declare variables
 string first, last;
-
+int lucky;
   //get user input
 cout<< "Please enter your first and last name.";
 cin>>first>>last;
 
   //tell fortune
 cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"." "here is your fortune...";
-
+lucky=first.length();
+cout<<"Your lucky number is " <<lucky<<endl;
   return 0;
 }

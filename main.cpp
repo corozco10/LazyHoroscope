703ad77ac17c41c700ce5093dbbba5c0826517d9
corozco10 <corozco10@mail.csuchico.edu>
2018-02-15 12:08:19 -0800

    Vowels

--- main.cpp
+++ main.cpp
@@ -17,26 +17,26 @@ int main()
         cout<< "What is your last name?\n";
         cin>>last;
         //tell fortune
-        cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"." "here is your fortune...\n";
+        cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"." ",here is your fortune...\n";
         lucky=first.length();
-        cout<<"Your lucky number is " <<lucky<<endl;
+        cout<<"your lucky number is " <<lucky<<endl;
         firstlet=first[0];
         if(firstlet == 65||firstlet == 69||firstlet == 73||firstlet == 79||firstlet == 85||firstlet == 97||firstlet == 101||firstlet == 105||firstlet == 111||firstlet == 117)
         {
-                cout<< "You are destined to be famous!\n";
+                cout<< "you are destined to be famous!\n";
         }
         else if((firstlet>65 &&firstlet<90) ||(firstlet>97 &&firstlet<122))
         {
-                cout<< "You should keep a low profile.\n";
+                cout<< "you should keep a low profile.\n";
         }
         lastlet=last[last.length()-1];
         if(lastlet == 97||lastlet == 101||lastlet == 105||lastlet == 111||lastlet == 117)
         {
-                cout<< "You have already met your true love.\n";
-                cout<< "Have a good day!\n";
+                cout<< "you have already met your true love.\n";
+                cout<< "have a good day!\n";
       }
       else
-          {      cout<< "Have a good day!\n";
+          {      cout<< "have a good day!\n";
            }    
                return 0;
   }
